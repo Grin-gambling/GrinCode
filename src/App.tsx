3066a321fef826998cd12c479424b47c49708a1b
Mina Bakrac <minabakrac@Mac.grinnell.edu>
2026-03-02 16:43:02 -0600

    Initial React + Vite + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Project Starter</h1>
+      <p>React + TypeScript is working.</p>
+    </div>
+  );
+}
+
+export default App;
