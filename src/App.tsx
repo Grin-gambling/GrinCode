b89c42754f50ef6f918c8fd2dc314e7fc58a4d02
Mina Bakrac <minabakrac@Minas-MacBook-Air.local>
2026-03-23 04:02:40 -0700

    Fonts adjusted, basic page style, non-functional buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import banner from "./components/gambling-banner.jpg";
+import Button from './button';
 
-function App() {
+export default function App() {
   return (
     <div>
-      <h1>Project Starter</h1>
-      <p>React + TypeScript is working.</p>
+
+    <div className="banner">
+      <img 
+        src={banner} 
+        alt="Website Banner" 
+      />
+      <h1>G R I N G A M B L I N G</h1>
+    </div>
+      
+      
+    <div className="button-area">
+      <Button
+        backgroundColor="#DA291C"
+        textColor="white"
+        fontSize={18}
+        pillShape
+      >
+        Create Bet
+      </Button>
+    </div>
+
+      <p> Tuition doesn't gamble away itself Grinnellians do har har</p>
     </div>
   );
 }
 
-export default App;
+
+
+
+
--- src/button.tsx
+++ src/button.tsx
@@ -18,9 +18,12 @@ export default function Button({
             backgroundColor: backgroundColor,
             color: textColor,
             fontSize: fontSize,
+            fontFamily: "Futura, sans-serif",
             borderRadius: pillShape ? "999px" : "4px",
             padding: "10px 16px",
+            border: "none", // removes blue outline when hovering
             cursor: "pointer",
+            outline: "none" // removes blue outline when hover
         }}>
             {children}
         </button>
